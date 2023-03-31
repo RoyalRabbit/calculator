77d907035048ac9af4edc82b76c867a94ba7a9bd
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-31 10:29:23 -0400

    Add buttons and css styling

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log('hello world')
+// ==========================================================================================
+
+
+// =============================================
+// Run Code
+// =============================================
+
+
+
+// Initialize Variables
+let numberOne = 0;
+let numberTwo = 0;
+let operatorSign = "";
+const display = document.querySelector('.display');
+
+
+//Console log testing
+console.log(add(1,3))
+console.log(subtract(1,3))
+console.log(multiply(1,3))
+console.log(divide(1,3))
+
+let displayText = document.createTextNode(`${add(1,3)}`)
+display.appendChild(displayText)
+
+
+
+
+
+
+
+
+
+// =============================================
+// End of Run Code
+// =============================================
+
+
+// ==========================================================================================
+
+
+// =============================================
+// Function List
+// =============================================
+
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+// =============================================
+// End of Function List
+// =============================================
