670d3d5b59b7002203227f824444734f063d6e56
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-04-03 11:01:06 -0400

    Add operator check logic against operators array

--- script.js
+++ script.js
@@ -10,9 +10,13 @@
 // Initialize Variables
 let numberOne = 0;
 let numberTwo = 0;
+const operators = ['+', '-', '*', '/']
 let operatorSign = "";
 const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('button')
+let displayValue = 0;
+// Set display to show 0 upon first loading
+display.innerText = displayValue;
+const buttons = document.querySelectorAll('button');
 
 //Console log testing
 console.log(add(1,3))
@@ -28,11 +32,18 @@ buttons.forEach(obj=>{
     // Add event listener on click
     obj.addEventListener('click', event=>{
         // Get value of the button that was clicked
-        let value=event.target.value
-        console.log(display.firstChild)
+        let value=event.target.value;
+        
+        // Check if button clicked was an operator
+        if (operators.includes(value)) {
+            operatorSign=(value);
+            console.log(operatorSign);
+            display.innerText = '';
+        }
         // Add a string child element with the value of the button clicked
         let displayText = document.createTextNode(`${value}`)
         display.appendChild(displayText)
+        displayValue=Number(display.innerText);
     })
 })
 
@@ -60,21 +71,47 @@ buttons.forEach(obj=>{
 // Function List
 // =============================================
 
+//Function to add two numbers
 function add(num1, num2) {
     return num1 + num2;
 };
 
+
+//Function to Subtract two numbers
 function subtract(num1, num2) {
     return num1 - num2;
 };
 
+
+// Function to multiply two numbers
 function multiply(num1, num2) {
     return num1 * num2;
 };
 
+
+// Function to divide two numbers
 function divide(num1, num2) {
     return num1 / num2;
 };
+
+// Function for operation
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '*':
+            return multiply(num1,num2);
+        case '-':
+            return subtract(num1,num2);
+        case '+':
+            return add(num1,num2);
+        case '/':
+            return divide(num1,num2);
+        default:
+            return 'Not valid operator'
+        
+
+        
+    }
+}
 
 // =============================================
 // End of Function List
